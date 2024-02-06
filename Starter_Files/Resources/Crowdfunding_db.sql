86b1bfb10e0ed31266756ffbadcc695ce9a8322c
Chisom413 <147779554+Chisom413@users.noreply.github.com>
2024-02-05 23:01:37 -0500

    Add files via upload

---
+++ Starter_Files/Resources/Crowdfunding_db.sql
@@ -0,0 +1,11 @@
+-- Select all records from Category
+SELECT * FROM Category;
+
+-- Select all records from Subcategory
+SELECT * FROM Subcategory;
+
+-- Select all records from Campaign
+SELECT * FROM Campaign;
+
+-- Select all records from Contacts
+SELECT * FROM Contacts;
